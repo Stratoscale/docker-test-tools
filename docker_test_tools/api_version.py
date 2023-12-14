e46be88c8169c1f7454a8e042b126b18b0db9830
Anton Khizunov <anton.khizunov@zadara.com>
2023-12-14 22:32:52 +0200

    NK-0000 fix py3 dependencies

--- docker_test_tools/api_version.py
+++ docker_test_tools/api_version.py
@@ -7,5 +7,13 @@ def get_server_api_version():
     server_versions = session.get('http+unix://%2Fvar%2Frun%2Fdocker.sock/version').json()
     return server_versions['ApiVersion']
 
+
+def get_server_api_version_new():
+    from docker import APIClient
+    client = APIClient(base_url='unix://var/run/docker.sock')
+    print(client.api_version)
+
+
 if __name__ == '__main__':
     print(get_server_api_version())
+    print(get_server_api_version_new())
