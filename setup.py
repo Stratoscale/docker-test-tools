332f0ae3a0f53a328dc239e987b09232c8df1de0
Anton Khizunov <anton.khizunov@zadarastorage.com>
2025-02-19 13:35:10 +0200

    NK-13440 fix docker version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     setup_requires=[
         'pbr>=5.5.1; python_version >= "3.0"',
-        'pbr==3.1.1 ; python_version < "3.0"'
+        'pbr==3.1.1; python_version < "3.0"'
     ],
-    pbr=True
+    pbr=True,
 )
